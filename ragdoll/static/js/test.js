b61febc2622389d9d16cbeb7f39b51bd4efc479f
Felix Palmer <felix.palmer@metaswitch.com>
2012-05-05 13:11:38 -0700

    Wrap code into doodle object

--- ragdoll/static/js/test.js
+++ ragdoll/static/js/test.js
@@ -17,154 +17,163 @@ b2CircleShape	= Box2D.Collision.Shapes.b2CircleShape,
 b2RevoluteJointDef	= Box2D.Dynamics.Joints.b2RevoluteJointDef;
 
 // Global shared stuff - 1 meter = 100 pixels
-var doodoll = {};
+var doodoll;
+doodoll = doodoll || {};
 
-var fixDef = new b2FixtureDef();
-fixDef.density = 10.0;
-fixDef.shape = new b2PolygonShape();
+doodoll.physics = function()
+{
 
-var bodyDef = new b2BodyDef();
-bodyDef.type = b2Body.b2_staticBody;
+    var fixDef = new b2FixtureDef();
+    var bodyDef = new b2BodyDef();
+    var scale = 0.5;
 
-var scale = 0.5;
+    var start = function() 
+    {	
+        fixDef.density = 10.0;
+        fixDef.shape = new b2PolygonShape();
 
-function Start() 
-{	
-    // Init stage & world
-    world = new b2World(new b2Vec2(0, 10),  true);
-    
-    var stage = new Stage("c");
-    stage.eventChildren = false;
-    stage.addEventListener(Event.ENTER_FRAME, onEF);
+        bodyDef.type = b2Body.b2_staticBody;
 
-    // background
-    var bg = new Bitmap( new BitmapData("winter2.jpg", 1) );
-    bg.scaleX = stage.stageWidth / 1024;
-    bg.scaleY = stage.stageHeight / 512;
-    stage.addChild(bg);
+        // Init stage & world
+        world = new b2World(new b2Vec2(0, 10),  true);
 
-    up = new b2Vec2(0, -700*scale);
+        var stage = new Stage("c");
+        stage.eventChildren = false;
+        stage.addEventListener(Event.ENTER_FRAME, onEF);
 
-    //create ground
-    bodyDef.position.Set(9, stage.stageHeight/100 + 1);
-    fixDef.shape.SetAsBox(10, 1);
-    world.CreateBody(bodyDef).CreateFixture(fixDef);
+        // background
+        var bg = new Bitmap( new BitmapData("winter2.jpg", 1) );
+        bg.scaleX = stage.stageWidth / 1024;
+        bg.scaleY = stage.stageHeight / 512;
+        stage.addChild(bg);
 
-    //left wall
-    bodyDef.position.Set(-1, 3);
-    fixDef.shape.SetAsBox(1, 100);
-    world.CreateBody(bodyDef).CreateFixture(fixDef);
+        up = new b2Vec2(0, -700*scale);
 
-    //right wall
-    bodyDef.position.Set(stage.stageWidth/100 + 1, 3);
-    fixDef.shape.SetAsBox(1, 100);
-    world.CreateBody(bodyDef).CreateFixture(fixDef);
+        //create ground
+        bodyDef.position.Set(9, stage.stageHeight/100 + 1);
+        fixDef.shape.SetAsBox(10, 1);
+        world.CreateBody(bodyDef).CreateFixture(fixDef);
 
-    for(var r = 0; r < 6; r++)
+        //left wall
+        bodyDef.position.Set(-1, 3);
+        fixDef.shape.SetAsBox(1, 100);
+        world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+        //right wall
+        bodyDef.position.Set(stage.stageWidth/100 + 1, 3);
+        fixDef.shape.SetAsBox(1, 100);
+        world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+        for(var r = 0; r < 6; r++)
+        {
+            addRagdollToWorld(world, stage);
+        }
+    };
+
+    var addRagdollToWorld = function(world, stage)
     {
-        addRagdollToWorld(world, stage);
-    }
-}
+        // Create a ragdoll
+        var personX = Math.random()*7;
+        var personY = (Math.random() - 1)*5;
+        var jointGap = 0.0 * scale;
+        var headSize = 1.5 * scale;
+        var bodyLength = 2.5 * scale;
+        var bodyWidth = 1.7 * scale;
+        var armLength = 1.5 * scale;
+        var armWidth = 0.3 * scale;
+        var legLength = 2.0 * scale;
+        var legWidth = 0.3 * scale;
 
-function addRagdollToWorld(world, stage)
-{
-    // Create a ragdoll
-    var personX = Math.random()*7;
-    var personY = (Math.random() - 1)*5;
-    var jointGap = 0.0 * scale;
-    var headSize = 1.5 * scale;
-    var bodyLength = 2.5 * scale;
-    var bodyWidth = 1.7 * scale;
-    var armLength = 1.5 * scale;
-    var armWidth = 0.3 * scale;
-    var legLength = 2.0 * scale;
-    var legWidth = 0.3 * scale;
-        
-    var head = [personX, personY, headSize, headSize]; // x, y, w, h
-    var body = [head[0], head[1] + head[3] + jointGap, bodyWidth, bodyLength];
-    var leftArm = [body[0] - (armLength + jointGap), body[1], armLength, armWidth];
-    var rightArm = [body[0] + body[2] + jointGap, body[1], armLength, armWidth];    
-    var leftLeg = [body[0] + 0.1, body[1] + body[3] + jointGap, legWidth, legLength];
-    var rightLeg = [body[0] + body[2] - (legWidth + 0.1), body[1] + body[3] + jointGap, legWidth, legLength];    
+        var head = [personX, personY, headSize, headSize]; // x, y, w, h
+        var body = [head[0], head[1] + head[3] + jointGap, bodyWidth, bodyLength];
+        var leftArm = [body[0] - (armLength + jointGap), body[1], armLength, armWidth];
+        var rightArm = [body[0] + body[2] + jointGap, body[1], armLength, armWidth];    
+        var leftLeg = [body[0] + 0.1, body[1] + body[3] + jointGap, legWidth, legLength];
+        var rightLeg = [body[0] + body[2] - (legWidth + 0.1), body[1] + body[3] + jointGap, legWidth, legLength];    
 
-    var headBody = addRectToWorld(head, world, stage);
-    var bodyBody = addRectToWorld(body, world, stage);
-    var leftArmBody = addRectToWorld(leftArm, world, stage);
-    var rightArmBody = addRectToWorld(rightArm, world, stage);
-    var leftLegBody = addRectToWorld(leftLeg, world, stage);
-    var rightLegBody = addRectToWorld(rightLeg, world, stage);
-    
-    // Link the wee lad up
-    var jointDef = new b2RevoluteJointDef();
-    jointDef.enableLimit = true;
-    
-    // Join head & body
-    jointDef.lowerAngle = -Math.PI/6; jointDef.upperAngle = Math.PI/6;
-    jointDef.Initialize(headBody, bodyBody, new b2Vec2(body[0] + body[2]/2, body[1]));
-    world.CreateJoint(jointDef);
-    
-    // Add arms
-    jointDef.Initialize(leftArmBody, bodyBody, new b2Vec2(body[0], body[1] + leftArm[3]/2));
-    world.CreateJoint(jointDef);
-    jointDef.Initialize(rightArmBody, bodyBody, new b2Vec2(body[0] + body[2], body[1] + rightArm[3]/2));
-    world.CreateJoint(jointDef);
+        var headBody = addRectToWorld(head, world, stage);
+        var bodyBody = addRectToWorld(body, world, stage);
+        var leftArmBody = addRectToWorld(leftArm, world, stage);
+        var rightArmBody = addRectToWorld(rightArm, world, stage);
+        var leftLegBody = addRectToWorld(leftLeg, world, stage);
+        var rightLegBody = addRectToWorld(rightLeg, world, stage);
 
-    // Add legs
-    jointDef.Initialize(leftLegBody, bodyBody, new b2Vec2(leftLeg[0] + leftLeg[2]/2, body[1] + body[3]));
-    world.CreateJoint(jointDef);
-    jointDef.Initialize(rightLegBody, bodyBody, new b2Vec2(rightLeg[0] + rightLeg[2]/2, body[1] + body[3]));
-    world.CreateJoint(jointDef);
-    
-    world.ClearForces();
-}
+        // Link the wee lad up
+        var jointDef = new b2RevoluteJointDef();
+        jointDef.enableLimit = true;
 
-/**
-* Returns a body object (used for joints)
-*/
-function addRectToWorld(rect, world, stage)
-{
-    var vertices = [new b2Vec2(0,0), new b2Vec2(rect[2],0), new b2Vec2(rect[2],rect[3]), new b2Vec2(0,rect[3])];
-    fixDef.shape.SetAsArray(vertices, 4);            
-    bodyDef.position.Set(rect[0], rect[1]);
-    bodyDef.type = b2Body.b2_dynamicBody;
+        // Join head & body
+        jointDef.lowerAngle = -Math.PI/6; jointDef.upperAngle = Math.PI/6;
+        jointDef.Initialize(headBody, bodyBody, new b2Vec2(body[0] + body[2]/2, body[1]));
+        world.CreateJoint(jointDef);
 
-    var body = world.CreateBody(bodyDef);
-    body.CreateFixture(fixDef);
-    bodies.push(body);
+        // Add arms
+        jointDef.Initialize(leftArmBody, bodyBody, new b2Vec2(body[0], body[1] + leftArm[3]/2));
+        world.CreateJoint(jointDef);
+        jointDef.Initialize(rightArmBody, bodyBody, new b2Vec2(body[0] + body[2], body[1] + rightArm[3]/2));
+        world.CreateJoint(jointDef);
 
-    // For rendering
-    var bd = new BitmapData("box.jpg", 1);
-    var box = new Bitmap(bd);
-    box.scaleX = rect[2]/2; box.scaleY = rect[3]/2;
-    box.addEventListener(MouseEvent.MOUSE_OVER, Jump);
-    stage.addChild(box);
-    boxes.push(box);
+        // Add legs
+        jointDef.Initialize(leftLegBody, bodyBody, new b2Vec2(leftLeg[0] + leftLeg[2]/2, body[1] + body[3]));
+        world.CreateJoint(jointDef);
+        jointDef.Initialize(rightLegBody, bodyBody, new b2Vec2(rightLeg[0] + rightLeg[2]/2, body[1] + body[3]));
+        world.CreateJoint(jointDef);
 
-    return body;
-}
+        world.ClearForces();
+    };
 
-function onEF(e) 
-{
-    world.Step(1 / 60,  3,  3);
-    world.ClearForces();
-
-    var p, body, box, coef = 180/Math.PI;
-    for(var i=0; i<boxes.length; i++)
+    /**
+    * Returns a body object (used for joints)
+    */
+    var addRectToWorld = function(rect, world, stage)
     {
-        body = bodies[i];
-        box  = boxes [i];
-        p = body.GetPosition();
-        box.x = p.x *100;
-        box.y = p.y *100;
-        box.rotation = body.GetAngle()*coef;
-    }
-}
+        var vertices = [new b2Vec2(0,0), new b2Vec2(rect[2],0), new b2Vec2(rect[2],rect[3]), new b2Vec2(0,rect[3])];
+        fixDef.shape.SetAsArray(vertices, 4);            
+        bodyDef.position.Set(rect[0], rect[1]);
+        bodyDef.type = b2Body.b2_dynamicBody;
 
-function Jump(e)
-{
-    console.log("Jump!");
-    var i = boxes.indexOf(e.target);
-    bodies[i].ApplyImpulse(up, bodies[i].GetWorldCenter());
-}
+        var body = world.CreateBody(bodyDef);
+        body.CreateFixture(fixDef);
+        bodies.push(body);
 
-window.onload = Start;
+        // For rendering
+        var bd = new BitmapData("box.jpg", 1);
+        var box = new Bitmap(bd);
+        box.scaleX = rect[2]/2; box.scaleY = rect[3]/2;
+        box.addEventListener(MouseEvent.MOUSE_OVER, Jump);
+        stage.addChild(box);
+        boxes.push(box);
+
+        return body;
+    };
+
+    var onEF = function(e) 
+    {
+        world.Step(1 / 60,  3,  3);
+        world.ClearForces();
+
+        var p, body, box, coef = 180/Math.PI;
+        for(var i=0; i<boxes.length; i++)
+        {
+            body = bodies[i];
+            box  = boxes [i];
+            p = body.GetPosition();
+            box.x = p.x *100;
+            box.y = p.y *100;
+            box.rotation = body.GetAngle()*coef;
+        }
+    };
+
+    var Jump = function(e)
+    {
+        console.log("Jump!");
+        var i = boxes.indexOf(e.target);
+        bodies[i].ApplyImpulse(up, bodies[i].GetWorldCenter());
+    };
+
+    return {
+        start : start  
+    };
+}();
+
+window.onload = doodoll.physics.start;
