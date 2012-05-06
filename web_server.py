a15a364daa5c332248829b04be02d9478b29d8a2
Felix Palmer <felix.palmer@metaswitch.com>
2012-05-05 17:42:05 -0700

    Basic web server

---
+++ web_server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+from flask import Flask
+
+app = Flask(__name__)
+
+# Real basic static web server
+if app.config['DEBUG']:
+    from werkzeug import SharedDataMiddleware
+    import os
+    app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+              '/': os.path.join(os.path.dirname(__file__), '/')
+                  })
+                  
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
